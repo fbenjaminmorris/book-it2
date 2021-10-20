b83e841d5011d439311418c40110baf236ec4379
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-10-20 17:36:00 -0400

    update

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./server/utils/auth');
 
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const db = require('./server/config/connection');
+const routes = require('./server/routes');
 
 
 //import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./server/schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
